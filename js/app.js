4f1c8930e8e04b406d055c3f3471d9919cb5ce90
Sybren Keizer <92291687+sybrenkeizer@users.noreply.github.com>
2022-10-13 21:23:41 +0545

    Add recipe copy functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 // TODO - BUG: Meal selection not working if h3 inside the .meal-info is clicked. Need to find an elegant way around it without multiplying the meal id on various elements inside .meal, in order to fetch correct meal data.
-// TODO - ENHANCEMENT: Make bottom meal items in meal container faded out. Upon scrolling have them fade in.
 // TODO - BUG: Look into youtube video embedding. Error: Cross-Origin Read Blocking (CORB).
-
+// TODO - ENHANCEMENT: Make bottom meal items in meal container faded out. Upon scrolling have them fade in.
 
 
 const searchEl = document.getElementById('search-input');
@@ -11,6 +10,7 @@ const mealEl = document.querySelector('.meal');
 const sidebarEl = document.getElementById('sidebar');
 const sidebarBackgroundEl = document.getElementById('sidebar-background');
 const brandImgEl = document.getElementById('brand-img');
+const notificationContainer = document.getElementById('notification-container');
 
 // FUNCTIONS
 const fetchMealBySearch = (e) => {
@@ -82,14 +82,27 @@ const populateSidebar = (meal) => {
       <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
     </div>
     <div class="ingredient-container">
-        <h3 class="ingredient-title">Ingredients</h3>
-        <ul class="ingredient-list">
-          ${ingredients.map(ing => `<li class="ingredient-list-item">${ing}</li>`).join('')}
+        <h3 class="ingredients-title">Ingredients</h3>
+        <a class="copy-btn copy-ingredients" id="copy-ingredients-btn" title="Copy text">
+          <i class="fa-solid fa-copy fa-lg"></i>
+        </a>
+        <ul class="ingredients-list">
+          ${ingredients.map(ing => `<li class="ingredients-list-item">${ing}</li>`).join('')}
         </ul>
       </div>
       <div class="instruction-container">
         <h3 class="instructions-title">Instructions</h3>
-        <p class="instructions-text">${meal.strInstructions}</p>
+        <a class="copy-btn copy-instructions" id="copy-instructions-btn" title="Copy text">
+          <i class="fa-solid fa-copy fa-lg"></i>
+        </a>
+        <ol class="instructions-list">${
+          meal.strInstructions
+            .slice(0, -1)
+            .split('.')
+            .map((step) => `<li>${step}.</li>`)
+              .join('')
+        }
+        </ol>
       </div>
       <div class="video-container">
         <h3 class="video-title">Video</h3>
@@ -111,11 +124,11 @@ const getMealId = (e) => e.target.parentElement.dataset.mealid;
 const showSidebar = () => {
   sidebarEl.classList.add('show');
   sidebarBackgroundEl.classList.add('darken-bg');
-
 }
 
 const hideSidebar = (e) => {
-  if (e.target.parentElement.classList.contains('close-sidebar-btn')) {
+  if (e.target.parentElement.classList.contains('close-sidebar-btn')
+  || e.target.classList.contains('sidebar-background')) {
     sidebarEl.classList.remove('show');
     sidebarBackgroundEl.classList.remove('darken-bg');
   }
@@ -124,7 +137,39 @@ const hideSidebar = (e) => {
 const hideLogo = () => brandImgEl.classList.add('hide');
 const showLogo = () => brandImgEl.classList.remove('hide');
 
+const copyToClipboard = (string) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+    return navigator.clipboard.writeText(string);
+  }
+  return Promise.reject('The Clipboard API is not available')
+}
+
+const copyIngredients = (e) => {
+  if (e.target.parentElement.classList.contains('copy-ingredients')) {
+    const list = e.target.parentElement.nextElementSibling;
+    const textToCopy = Array.from(list.children)
+                              .map(li => `${li.textContent}`.trim() + '\n')
+                              .join('')
+                              .trim();
+    copyToClipboard(textToCopy);
+  }
+}
+
+const copyInstructions = (e) => {
+  if (e.target.parentElement.classList.contains('copy-instructions')) {
+    const textToCopy = e.target.parentElement.nextElementSibling.textContent.trim();
+    copyToClipboard(textToCopy);
+    showNotification('Instructions Copied')
+  }
+}
+
+
 // EVENT LISTENERS
 searchEl.addEventListener('input', fetchMealBySearch);
 mealsContainerEl.addEventListener('click', selectMeal);
-sidebarEl.addEventListener('click', hideSidebar);
+sidebarEl.addEventListener('click', (e) => {
+  hideSidebar(e);
+  copyIngredients(e);
+  copyInstructions(e);
+});
+sidebarBackgroundEl.addEventListener('click', hideSidebar);
